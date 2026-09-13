52272979c4f710cd7fe62c65b05ed97c65ea55a6
gargbhavya2265 <bhavyagarg2265@gmail.com>
2026-09-13 18:57:19 +0530

    Fix admin Kafka authentication

--- admin-service/src/config/index.js
+++ admin-service/src/config/index.js
@@ -5,10 +5,11 @@ const config = {
      LOG_LEVEL: process.env.LOG_LEVEL || 'info',
      KAFKA_BROKER: process.env.KAFKA_BROKER,
      KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID,
+     KAFKA_USERNAME: process.env.KAFKA_USERNAME,
+     KAFKA_PASSWORD: process.env.KAFKA_PASSWORD,
      DATABASE_URL: process.env.DATABASE_URL,
      ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
-
      INTERNAL_SERVICE_KEY: process.env.INTERNAL_SERVICE_KEY,
 }
 
-module.exports = {config};
+module.exports = { config };
