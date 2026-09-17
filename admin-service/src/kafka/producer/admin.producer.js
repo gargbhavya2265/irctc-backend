bc139b7aee57e71502c09079708ff621ed9b15e9
gargbhavya2265 <bhavyagarg2265@gmail.com>
2026-09-17 21:45:25 +0530

    Fix shared kafka topics import path

--- admin-service/src/kafka/producer/admin.producer.js
+++ admin-service/src/kafka/producer/admin.producer.js
@@ -1,8 +1,7 @@
 const { producer, connectProducer } = require('../../config/kafka');
 
 const logger = require('../../config/logger');
-
-const { KAFKA_TOPICS } = require('../../../../shared/constants/kafka-topics');
+const { KAFKA_TOPICS } = require('../../../shared/constants/kafka-topics');
 
 class AdminProducer {
   constructor() {
